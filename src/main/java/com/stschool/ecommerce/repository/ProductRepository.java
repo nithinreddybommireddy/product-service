b6c884a7270fff0a4b6a764e3faa62b774c4c63c
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-06-21 20:36:59 +0530

    feat:Product Repository

---
+++ src/main/java/com/stschool/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.stschool.ecommerce.repository;
+
+import com.stschool.ecommerce.document.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+    Optional<Product> findByName(String name);
+
+}
+
+
