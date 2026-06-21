a4bdbb125f4cfd1b10aac53ba182d3f696e0b6a4
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-06-21 20:34:30 +0530

    feat:Api Response Dto

---
+++ src/main/java/com/stschool/ecommerce/dto/response/ApiResponseDto.java
@@ -0,0 +1,18 @@
+package com.stschool.ecommerce.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponseDto<T> {
+
+    private boolean success;
+    private String message;
+    private int code;
+    private T data;
+}
